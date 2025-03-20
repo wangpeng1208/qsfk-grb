4264efc9fcd801ed3da6e109daceb9ed094e7e1f
契约 <286204069@qq.com>
2025-03-21 07:20:53 +0800

    版本号更新至 v1.1.1

--- service/config/site.php
+++ service/config/site.php
@@ -1,18 +1,12 @@
 <?php
 return [
     // 当前版本号
-    'version'     => getenv('APP_VERSION') ?: 'v1.1.0',
-    // 是否是演示模式
-    'is_demo'     => false,
-    // 远程api地址
-    'api_url'     => [
-        'https://www.qqss.net',
-    ],
+    'version'     => getenv('APP_VERSION') ?: 'v1.1.1',
     // 使用协议官方网址
     'api_license' => [
         [
             'name' => '官网链接',
-            'url'  => 'https://www.qqss.net',
+            'url'  => getenv('API_LICENSE') ?: 'https://www.qqss.net',
         ],
     ],
 ];
